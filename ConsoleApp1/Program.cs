9db704c77a4ce309dfa9fc842e61ace648c0e0dd
engin nACAR <nacarenginq@gmail.com>
2022-07-10 15:32:25 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+
+
+ProductManager productManager = new ProductManager(new EfProductDal());
+
+foreach (var item in productManager.GetAll())
+{
+    Console.WriteLine(item.ProductName);
+}
+Console.ReadLine(); 
